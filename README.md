666f4b75f7e074c4d55e2bb8794b877d6ce9b28e
ed100miles <ed100miles@gmail.com>
2021-05-28 13:37:50 +0100

    -Made postcode.js import dynamically -Added link to google.maps -Added CSS -Fixed README markdown

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
-# NearestVaccineCentre
-# (https://ed100miles.github.io/NearestVaccineCentre/)
-### Finds your nearest Covid-19 vaccine centre (England Only - sorry)
+# NearestVaccineCentre 
 
-If you look online, the only way to find your closest vaccine centre is to look at a very old-school OS map drawn up by the Ministry of Defence. Or you can scroll through the list of ~2000 vaccine centres until you find one close to you. This isn't very convenient. 
+## (https://ed100miles.github.io/NearestVaccineCentre/) 
+
+## Finds your nearest Covid-19 vaccine centre 
+
 ---
 
-## Enter Python! (and an exciting evening of scripting)
+If you look online, the only way to find your closest vaccine centre is to look at a very old-school OS map drawn up by the Ministry of Defence. Or you can scroll through the list of ~2000 vaccine centres until you find one close to you. This isn't very convenient. 
+
+---
+
+## Enter Python! (then JS)
 
 Two datasets were used for this project. 
 
-1. The list of Covid-19 vaccination sites available on the NHS England website. (correct as of 21 May 2021)
+1. The list of Covid-19 vaccination sites available on the NHS England website (correct as of 21 May 2021).
 2. A list of 1.7m postcodes in the UK and their approximate longitudes and latitudes.
 
 Longs and Lats were then added to the list of vaccine sites. 
+
 ---
 
 ## Usage
 
-The user enters their postcode, and their nearest vaccine centre is printed to console - simple! I plan on adding an MVP front end soon. 
+The user enters their postcode, and their nearest vaccine centre is displayed. Also a link to google maps showing the location of the vaccine centre. 
+
 ---
 
 ## How it works
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import {POSTCODES} from './data/postcode.js';
 import {VAC_SITES} from './data/vac_sites.js';
-// console.log(POSTCODES)
-// console.log('all done!')
 
 const USER_PCODE = document.querySelector('#user_postcode');
 const POSTCODE_BTN = document.querySelector('#postcode_btn');
 const NEAREST_CENTRE_DIV = document.querySelector('#nearest_centre_div');
+const LINK_DIV = document.querySelector('#link-div')
+
+let user_lat
+let user_long
+let site_lat
+let site_long
+let closest_site
+let closest_site_key
 
 function getHypot(user_lat, user_long, site_lat, site_long){
     let lat_dist = user_lat - site_lat;
@@ -13,35 +18,37 @@ function getHypot(user_lat, user_long, site_lat, site_long){
     return Math.sqrt(lat_dist**2 + long_dist**2)
 }
 
-let user_lat
-let user_long
-let site_lat
-let site_long
-let closest_site
 function findCentre(e){
     e.preventDefault();
-    let user_postcode = USER_PCODE.value;
-    // console.log(user_postcode)
-    for(let postcode of POSTCODES){
-        if(postcode[0] == user_postcode){
-            user_long = postcode[1];
-            user_lat = postcode[2];
-        }
-    }
-    let smallest_distance = getHypot(user_lat, user_long, 1000, 1000);
-        // console.log(smallest_distance)
-    for(let [key, value] of Object.entries(VAC_SITES)){
-        site_lat = value[4]
-        site_long = value[3]
-        let site_distance = getHypot(user_lat, user_long, site_lat, site_long)
-        if(site_distance < smallest_distance){
-            smallest_distance = site_distance;
-            closest_site = VAC_SITES[key]
-        }
-    };
-    console.log(closest_site)
-    NEAREST_CENTRE_DIV.innerHTML = `<h3>${closest_site[0]}</h3><p>${closest_site[1]}</p>`
+    NEAREST_CENTRE_DIV.innerHTML = 'Finding your nearest vaccine centre. Please wait...'
+    import('/data/postcode.js')
+        .then((POSTCODES) => {
+            POSTCODES = POSTCODES.POSTCODES
+            let user_postcode = USER_PCODE.value;
+            // console.log(user_postcode)
+            for(let postcode of POSTCODES){
+                if(postcode[0] == user_postcode){
+                    user_long = postcode[1];
+                    user_lat = postcode[2];
+                }
+            }
+            let smallest_distance = getHypot(user_lat, user_long, 1000, 1000);
+                // console.log(smallest_distance)
+            for(let [key, value] of Object.entries(VAC_SITES)){
+                site_lat = value[4]
+                site_long = value[3]
+                let site_distance = getHypot(user_lat, user_long, site_lat, site_long)
+                if(site_distance < smallest_distance){
+                    smallest_distance = site_distance;
+                    closest_site = VAC_SITES[key]
+                    closest_site_key = key
+                }
+            };
+            let stite_name_for_URL = closest_site[0].replace(/\s/g, '+');
+            let site_postcode_for_URL = closest_site_key.replace(/\s/g, '');
+            NEAREST_CENTRE_DIV.innerHTML = `<h3>${closest_site[0]}</h3><p>${closest_site[1]}</p>`;
+            LINK_DIV.innerHTML = `<a href="https://www.google.com/maps/search/?api=1&query=${stite_name_for_URL}+${site_postcode_for_URL}">Click for map</a>`
+        })
 };
 
 POSTCODE_BTN.addEventListener('click', findCentre);
-
